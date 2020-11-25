c235423a4524e46238b50cf85df40bcd92ee3e98
keno <keno123@126.com>
2020-11-26 00:05:58 +0800

    添加了GRPC C#  控制台客户端

---
+++ grpcServer-CSharp/Services/MyEmployeeService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcServer.Web.Protos;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcServer.Web
+{
+    public class MyEmployeeService:EmployeeService.EmployeeServiceBase
+    {
+        private readonly ILogger<MyEmployeeService> logger;
+
+        public MyEmployeeService(ILogger<MyEmployeeService> logger) {
+            this.logger = logger;
+        }
+
+        public override Task<EmployeeResponse> GetByNo(GetByNoRequest request, ServerCallContext context)
+        {
+            var employee = new Employee
+            {
+                Id = 101,
+                No = request.No,
+                FirstName = $"FirstName ${request.No}",
+                LastName = $"LastName ${request.No}",
+
+            };
+            var response = new EmployeeResponse {
+                Employee = employee
+            };
+
+            return Task.FromResult(response);
+      
+        }
+    }
+}
